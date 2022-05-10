03a2a34e955edc526041f2d3077252a4cfa025c2
Kinga Kohari <kinguca@yahoo.com>
2022-05-10 11:56:25 +0200

    Timing issues

--- src/Subscription.jsx
+++ src/Subscription.jsx
@@ -1,60 +1,51 @@
 import React, {useState, useEffect} from 'react'
-import LoadingMask from './LoadingMask';
+import LoadingMask from './LoadingMask'
 import Textfield from '@mui/material/TextField'
-import Button from '@mui/material/Button';
+import Button from '@mui/material/Button'
 
 const Subscription = () => {
 
-    const [isActive, setActive] = useState("false");
+    const [isActive, setActive] = useState("false")
     const [input, setInput] = useState("")
     const [loading, setLoading] = useState(false)
 
     useEffect (() => {
-        setTimeout(() => {
-            setActive("false");
-            setTimeout(() => setActive("true"), 10000)
-        })
-        
+        setActive("false");
+        setTimeout(() => setActive("true"), 10000)
     },[])
 
 
     async function requestHandler(){
 
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: { 'email': 'what@user.wrote' }
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: { 'email': 'what@user.wrote' }
+        }
+
+        const response = await fetch("https://demoapi.com/api/series/newsletter", requestOptions)
+        const resJSON = await response.json()
+        console.log(resJSON);
+        console.log("Subscribed");
+        setLoading(false)    
     }
 
-    const response = await fetch("https://demoapi.com/api/series/newsletter", requestOptions)
-    const resJSON = await response.json()
-    console.log(resJSON);
-    console.log("Subscribed");
-    setLoading(false)
-    
-    }
-
-    useEffect( () => {
+    useEffect (() => {
         setLoading(true)
         requestHandler()
+        setActive("true");
+        setTimeout(() => setActive("false"), 5000)
       }, [])
-
-      useEffect (() => {
-        setTimeout(() => {
-            setActive("true");
-            setTimeout(() => setActive("false"), 5000)
-        })
-        
-    },[])
 
 
     return (
-    <div isactive={isActive}>
+    <div className={isActive ? "subscribe" : "subscribe_hidden"}>
         <h2>Subscribe to our newsletter</h2>
-        <form loading={loading}>{(loading) => <LoadingMask />} 
+        {loading ? <LoadingMask /> :
+        <form>
             <Textfield placeholder="Email address" variant="filled" value={input} onChange={ ({target}) => setInput(target.value)} required />
             <Button variant="contained" style={{height:50}} onClick={requestHandler}>Go</Button>
-        </form>
+        </form>}
     </div>
     
     )
